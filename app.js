5dfa250697275414bbbc623d3b863abf74090d59
Burlak Ilia <i.burlak@it-online.ru>
2013-09-21 10:48:52 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: i.burlak
+ * Date: 21.09.13
+ * Time: 10:27
+ * To change this template use File | Settings | File Templates.
+ */
+console.log('start');
